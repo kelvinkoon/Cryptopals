17d1bc6e206210c96441884faf14bd6014717793
kelvinkoon <kkoon@shaw.ca>
2021-05-25 21:49:11 -0700

    Re-add challenge 1

---
+++ Set1/challenge01.py
@@ -0,0 +1,20 @@
+import binascii
+import base64
+
+input_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+expected_str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+def convertHexToBase64(hex_enc_str):
+    # Decode hex to ASCII
+    ascii_bytes = binascii.unhexlify(hex_enc_str.encode())
+    # Encode ASCII to base64
+    base64_bytes = base64.b64encode(ascii_bytes)
+    base64_str = base64_bytes.decode("utf-8")
+    return base64_str
+
+def main():
+    output_str = convertHexToBase64(input_str)
+    assert output_str == expected_str
+
+if __name__ == "__main__":
+    main()
