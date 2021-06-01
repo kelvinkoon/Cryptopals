c7cb86e75b8a8887403ab579c2af4de2065051c6
kelvinkoon <kkoon@shaw.ca>
2021-06-01 00:17:47 -0700

    Fix challenge 9 formatting

--- Set2/challenge09.py
+++ Set2/challenge09.py
@@ -1,6 +1,7 @@
 INPUT_STR = "YELLOW SUBMARINE"
 EXPECTED_BYTES = b"YELLOW SUBMARINE\x04\x04\x04\x04"
 
+
 def addPKCS7Padding(ascii_bytes: bytes, block_size: int):
     """
     Returns the ASCII byte array padded with PKCS#7 padding based on block size
@@ -11,14 +12,16 @@ def addPKCS7Padding(ascii_bytes: bytes, block_size: int):
     # Determine amount of padding required
     num_padding = block_size - (len(ascii_bytes) % block_size)
     padded_ascii_bytes = ascii_bytes
-    padded_ascii_bytes += num_padding*bytes([num_padding])
-    
+    padded_ascii_bytes += num_padding * bytes([num_padding])
+
     return padded_ascii_bytes
+
 
 def main():
     ascii_bytes = INPUT_STR.encode("utf-8")
     padded_ascii_bytes = addPKCS7Padding(ascii_bytes, 20)
     assert padded_ascii_bytes == EXPECTED_BYTES
+
 
 if __name__ == "__main__":
     main()
