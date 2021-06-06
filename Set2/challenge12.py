048e341ec3e007d1b730341d46215dc48c9bac7e
kelvinkoon <kkoon@shaw.ca>
2021-06-06 00:57:13 -0700

    Add challenge 12 solution

--- Set2/challenge12.py
+++ Set2/challenge12.py
@@ -5,77 +5,84 @@ from Set1.challenge07 import *
 import base64
 
 UNKNOWN_STRING = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+BLOCK_SIZE_MAX_GUESS = 20
 
-"""
-AES-128-ECB(your-string || unknown-string, random-key) 
-"""
-class ECBOracle():
+
+class ECBOracle:
+    """
+    An oracle using AES-ECB encryption
+    Initializes with a random AES key
+    encrypt() -> AES-128-ECB(your-string || unknown-string, random-key)
+    """
+
     def __init__(self):
         self.unknown_encrypted_bytes = base64.b64decode(UNKNOWN_STRING.encode("utf-8"))
         self.random_key = generateRandomAESKey()
-    
+
     def encrypt(self, plaintext_bytes):
         ecb_input_bytes = plaintext_bytes + self.unknown_encrypted_bytes
-        encrypted_bytes = encryptAES_ECBModePKCS7Padded(ecb_input_bytes, self.random_key)
+        encrypted_bytes = encryptAES_ECBModePKCS7Padded(
+            ecb_input_bytes, self.random_key
+        )
         return encrypted_bytes
+
 
 def breakOracleByteAtATimeSimple(ecb_oracle: ECBOracle) -> bytes:
     """
-    TODO: Write Javadoc
+    Returns the plaintext given an ECB Oracle
+
+    :param ecb_oracle The ECBOracle object specified in the challenge
     """
     # Feed identical bytes of known_plaintext_bytes to determine block size
-    # TODO: Implement logic for determining block size
-    block_size = 16
+    # Upon reaching end of block, PKCS#7 padding will add a new block
+    # The length of the new block is the block size
+    block_size = 0
+    prev_length_guess_oracle_bytes = len(ecb_oracle.encrypt(b"A"))
+    for i in range(2, BLOCK_SIZE_MAX_GUESS):
+        length_guess_input_byte = b"A" * i
+        curr_length_guess_oracle_bytes = len(
+            ecb_oracle.encrypt(length_guess_input_byte)
+        )
+        # Upon detecting a change in length, the block size is the difference thanks to padding
+        if prev_length_guess_oracle_bytes != curr_length_guess_oracle_bytes:
+            block_size = curr_length_guess_oracle_bytes - prev_length_guess_oracle_bytes
+            break
+        prev_length_guess_oracle_bytes = curr_length_guess_oracle_bytes
 
-    # Decrypt unknown bytes 1-byte-at-a-time
-    # Test decoding the first byte
-    # first_guess_input = b"A"*15
-    # first_guess = ecb_oracle.encrypt(first_guess_input) 
-    # print("FIRST GUESS")
-    # print(first_guess)
-    # print(len(first_guess))
-    # for guess_byte in range(0, 128):
-    #     curr_guess_bytes = b"A"*15 + bytes([guess_byte])
-    #     oracle_guess_block = ecb_oracle.encrypt(curr_guess_bytes)
-    #     # print(len(oracle_guess_block))
-    #     # print(bytes([guess_byte]))
-    #     # print(oracle_guess_block)
-    #     # print("---")
-    #     if oracle_guess_block[:16] == first_guess[:16]:
-    #         print("FOUND")
-    #         print(bytes([guess_byte]))
-    #         print(first_guess[:17])
-    #         print(oracle_guess_block[:17])
+    # Verify oracle is using ECB mode
+    ecb_guess_oracle_bytes = ecb_oracle.encrypt(b"A" * 64)
+    blocks = breakByteArrayIntoBlocks(ecb_guess_oracle_bytes, block_size)
+    if len(blocks) == len(set(blocks)):
+        raise Exception("Oracle is not encrypting with ECB mode")
 
-    # first_find = b"R"
-    # second_guess_input = b"A"*14
-    # second_guess = ecb_oracle.encrypt(second_guess_input)
-    # for guess_byte in range(0, 128):
-    #     curr_guess_bytes = b"A"*14 + first_find + bytes([guess_byte])
-    #     oracle_guess_block = ecb_oracle.encrypt(curr_guess_bytes)
-    #     if oracle_guess_block[:16] == second_guess[:16]:
-    #         print("FOUND AGAIN")
-    #         print(bytes([guess_byte]))
-    # TODO: Add logic to calculate
-    num_blocks = 10
+    # Ciphertext length can be calculated using block size
+    # `your-string || unknown-string` where `your-string` is the block size
+    oracle_guess_length = len(ecb_oracle.encrypt(b"A" * block_size))
+    num_blocks = oracle_guess_length // block_size
     result_bytes = b""
-    for i in range(0, 11):
-        print("Starting new block")
-        for j in range(1, block_size+1):
+
+    # Iterate through each block to decrypt for plaintext block
+    for i in range(0, num_blocks):
+        for j in range(1, block_size + 1):
             # Send one-byte-short input
-            one_byte_short_input_bytes = b"A"*(block_size - j) 
-            one_byte_short_oracle_bytes = ecb_oracle.encrypt(one_byte_short_input_bytes) 
-            # Try letters
+            one_byte_short_input_bytes = b"A" * (block_size - j)
+            one_byte_short_oracle_bytes = ecb_oracle.encrypt(one_byte_short_input_bytes)
+            # Brute-force characters until one-byte-short output matches the guess's current block
             for guess_byte in range(0, 128):
-                curr_guess_bytes = b"A"*(block_size - j) + result_bytes + bytes([guess_byte])
-                print(curr_guess_bytes)
+                curr_guess_bytes = (
+                    b"A" * (block_size - j) + result_bytes + bytes([guess_byte])
+                )
                 curr_guess_oracle_bytes = ecb_oracle.encrypt(curr_guess_bytes)
-                if one_byte_short_oracle_bytes[i*block_size:i*block_size + block_size] == curr_guess_oracle_bytes[i*block_size:i*block_size + block_size]:
-                    print("FOUND BYTE")
-                    print(bytes([guess_byte]))
-                    # Add to current block result
+                if (
+                    one_byte_short_oracle_bytes[
+                        i * block_size : i * block_size + block_size
+                    ]
+                    == curr_guess_oracle_bytes[
+                        i * block_size : i * block_size + block_size
+                    ]
+                ):
+                    # Add to the resulting byte array
                     result_bytes += bytes([guess_byte])
                     break
-    print("RESULT")
-    print(removePKCS7Padding(result_bytes))
 
+    return removePKCS7Padding(result_bytes)
--- Set2/challenge12_test.py
+++ Set2/challenge12_test.py
@@ -2,11 +2,11 @@ from shared.validation_functions import *
 from Set2.challenge12 import *
 from Set2.challenge11 import *
 
-PLAINTEXT_BYTES = b"A"*16
 UNKNOWN_STRING = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+EXPECTED_HASH_HEX_STR = "0f7a938a0a6fc97f763710454b139317f4db7ed2"
+
 
 def test_ECBDecryption():
-    plaintext_bytes = b"A"*16
     ecb_oracle = ECBOracle()
-    known_bytes = breakOracleByteAtATimeSimple(ecb_oracle)
-    assert False
+    plaintext_bytes = breakOracleByteAtATimeSimple(ecb_oracle)
+    assert hashBytesToSHA1Str(plaintext_bytes) == EXPECTED_HASH_HEX_STR
