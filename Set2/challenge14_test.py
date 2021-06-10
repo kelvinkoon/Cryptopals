ed29c98748a515f6fbd82362f747b65412e2eaa7
kelvinkoon <kkoon@shaw.ca>
2021-06-09 17:02:24 -0700

    Finish challenge14

--- Set2/challenge14_test.py
+++ Set2/challenge14_test.py
@@ -1,6 +1,12 @@
 from Set2.challenge14 import *
+from shared.validation_functions import *
+
+NUM_ITERATIONS = 100
+EXPECTED_HASH_HEX_STR = "0f7a938a0a6fc97f763710454b139317f4db7ed2"
+
 
 def test_breakOracleByteAtATimeHarder():
-    ecb_oracle = HarderECBOracle()
-    result_bytes = breakOracleByteAtATimeHarder(ecb_oracle) 
-    assert False
+    for i in range(NUM_ITERATIONS):
+        ecb_oracle = HarderECBOracle()
+        result_bytes = breakOracleByteAtATimeHarder(ecb_oracle)
+        assert hashBytesToSHA1Str(result_bytes) == EXPECTED_HASH_HEX_STR
